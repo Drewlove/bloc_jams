e3c1ab102737999e0eacec05eed8e1604ede46c0
Drew <Drew@Drews-MacBook-Air.local>
2017-01-26 09:24:17 -0500

    checkpoint-10

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,7 +1,5 @@
-// forEach loops through an array and calls a function on each element in the array
-
-var forEach = function(arrayName, func){
-    for(var i=0; i < arrayName.length; i++) {
-        func(i);
+function forEach(array, callback){
+    for(var i=0; i < array.length; i++) {
+        callback(array[i]);
     }
 }

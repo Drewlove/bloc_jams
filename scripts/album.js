81bac86a972ab839b9f92b9856ae9ae65fad707b
Drew <Drew@Drews-MacBook-Air.local>
2017-01-30 19:16:25 -0500

    checkpoint-12

--- scripts/album.js
+++ scripts/album.js
@@ -1,54 +1,40 @@
- var albumPicasso = {
-     name: 'The Colors',
+//Code from the beginning of Checkpoint 12
+
+var albumPicasso = {
+     title: 'The Colors',
      artist: 'Pablo Picasso',
      label: 'Cubism',
      year: '1881',
      albumArtUrl: 'assets/images/album_covers/01.png',
      songs: [
-         { name: 'Blue', length: '4:26' },
-         { name: 'Green', length: '3:14' },
-         { name: 'Red', length: '5:01' },
-         { name: 'Pink', length: '3:21'},
-         { name: 'Magenta', length: '2:15'}
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
      ]
  };
  
  // Another Example Album
  var albumMarconi = {
-     name: 'The Telephone',
+     title: 'The Telephone',
      artist: 'Guglielmo Marconi',
      label: 'EM',
      year: '1909',
      albumArtUrl: 'assets/images/album_covers/20.png',
      songs: [
-         { name: 'Hello, Operator?', length: '1:01' },
-         { name: 'Ring, ring, ring', length: '5:01' },
-         { name: 'Fits in your pocket', length: '3:21'},
-         { name: 'Can you hear me now?', length: '3:14' },
-         { name: 'Wrong phone number', length: '2:15'}
-     ]
- };
-
-// Assignment 11 Album
- var albumTest = {
-     name: 'JS The Night Away',
-     artist: 'Proper Noun and the Adjective Plural Nouns',
-     label: 'Subpop',
-     year: '2012 ',
-     albumArtUrl: 'assets/images/album_covers/22.png',
-     songs: [
-         { name: 'The Osh Gosh B Gosh Took My Baby Away', length: '3:01' },
-         { name: 'These Boots Were Made for Waddling', length: '2:48' },
-         { name: 'Playground Friction', v: '1:49'},
-         { name: 'Why?', length: '3:52' },
-         { name: 'Existential Pre-Nap Blues', length: '2:24'}
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
 
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -57,6 +43,8 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
+    var setCurrentAlbum = function(album) {
+     // #1
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
@@ -64,7 +52,7 @@ var createSongRow = function(songNumber, songName, songLength) {
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
  
      // #2
-     albumTitle.firstChild.nodeValue = album.name;
+     albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
@@ -73,21 +61,37 @@ var createSongRow = function(songNumber, songName, songLength) {
      albumSongList.innerHTML = '';
  
      // #4
-     for (i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+     for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
  
+//Code snippet added from Checkpoint 12
+
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+    
+
+// Album button templates
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
-     
-     var albums = [albumPicasso, albumMarconi, albumTest];
-     var index = 1;
-     albumImage.addEventListener("click", function(event) {
-         setCurrentAlbum(albums[index]);
-         index++;
-         if(index == albums.length) {
-             index = 0;
+
+     songListContainer.addEventListener('mouseover', function(event) {
+         // #1
+         console.log(event.target);
+         if (event.target.parentElement.className === 'album-view-song-item'){
+             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
          }
      });
-};
+    
+     for (var i=0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event){
+            //Selects first child element, which is the song-item-number element
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+    }
+}
+ 
+  
